4b36d862bbce441aa1b7030f3b690ec2f7a4a058
energizedNachos <16521108@mahasiswa.itb.ac.id>
2023-03-17 15:36:10 +0700

    docs: restructuring files

--- src/Map/Map.cs
+++ src/Map/Map.cs
@@ -1,14 +1,14 @@
 using System;
 namespace src{
     public class Maze{
-        private Cell[,]? cells;
-        private int size;
-        public Maze(int[,] maze){
-            for (int row = 0; row < size; row++){
-                for (int col = 0; col < size; col++){
-                    cells[row,col] = new Cell { Row = row, Col = col}; 
-                }
-            }
-        }
+        // private Cell[,]? cells;
+        // private int size;
+        // public Maze(int[,] maze){
+        //     for (int row = 0; row < size; row++){
+        //         for (int col = 0; col < size; col++){
+        //             cells[row,col] = new Cell { Row = row, Col = col}; 
+        //         }
+        //     }
+        // }
     }
 }
