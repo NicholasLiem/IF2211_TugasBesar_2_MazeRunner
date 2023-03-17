f99214cc3f0114a0481f626bca57a3b95c66af60
energizedNachos <16521108@mahasiswa.itb.ac.id>
2023-03-17 12:42:00 +0700

    docs: init

--- src/FileReader/FileController.cs
+++ src/FileReader/FileController.cs
@@ -7,20 +7,26 @@ namespace src{
             string basePath = Directory.GetCurrentDirectory();
             Console.WriteLine("Base directory: " + basePath);
 
-        //     Console.WriteLine("Enter the name of a text file in the 'test' folder:");
-        //     string fileName = Console.ReadLine();
-        //     string filePath = Path.Combine("..", "..", "test", fileName);
+            Console.WriteLine("Enter the name of a text file in the 'test' folder:");
+            string? fileName = Console.ReadLine();
 
-        //     if (File.Exists(filePath) && Path.GetExtension(filePath) == ".txt")
-        //     {
-        //         Console.WriteLine("Selected file: " + filePath);
-        //         string fileContents = File.ReadAllText(filePath);
-        //         Console.WriteLine("File contents: " + fileContents);
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine("Invalid file path or file type.");
-        //     }
+            while(fileName == null || fileName == ""){
+                Console.WriteLine("Please enter a valid file name:");
+                fileName = Console.ReadLine();
+            }
+
+            string filePath = Path.Combine(basePath, "test", fileName);
+
+            if (File.Exists(filePath) && Path.GetExtension(filePath) == ".txt")
+            {
+                Console.WriteLine("Selected file: " + filePath);
+                string fileContents = File.ReadAllText(filePath);
+                Console.WriteLine("File contents: " + fileContents);
+            }
+            else
+            {
+                Console.WriteLine("Invalid file path or file type.");
+            }
         }
     }
 }
