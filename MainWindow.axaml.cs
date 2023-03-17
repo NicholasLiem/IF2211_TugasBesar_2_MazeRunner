4bc75dd5f659f96dc25af7005238deb7c10fb846
energizedNachos <16521108@mahasiswa.itb.ac.id>
2023-03-17 15:29:59 +0700

    update: installing avalonia ui to project

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Tubes2_MazeRunner
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,41 @@
-﻿using System;
+﻿//using Avalonia;
+//using System;
 
-namespace src{
-    public class Program
+//namespace src{
+    //public class Program
+    //{
+        //public static void Main(string[] args)
+        //{
+          //  int [,] mapMatrix = new int[0,0];
+            //FileManager fileReader = new FileManager();
+            //Utils Util = new Utils();
+            //fileReader.ShowFilesInFolder();
+            //fileReader.ReadFile(ref mapMatrix);
+            //Util.PrintMap(ref mapMatrix);
+        //}
+
+    //}
+
+//}
+
+using Avalonia;
+using System;
+
+namespace Tubes2_MazeRunner
+{
+    internal class Program
     {
-        public static void Main(string[] args)
-        {
-            int [,] mapMatrix = new int[0,0];
-            FileManager fileReader = new FileManager();
-            Utils Util = new Utils();
-            fileReader.ShowFilesInFolder();
-            fileReader.ReadFile(ref mapMatrix);
-            Util.PrintMap(ref mapMatrix);
-        }
-    }
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
 
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
+    }
 }
