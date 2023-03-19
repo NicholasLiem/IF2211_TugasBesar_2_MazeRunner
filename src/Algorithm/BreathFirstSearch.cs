bfc669b271e2839710d1725a2964257b5878550f
HoseaNA <110534062+HoseaNA@users.noreply.github.com>
2023-03-19 18:52:46 +0700

    bfs

--- src/Algorithm/BreathFirstSearch.cs
+++ src/Algorithm/BreathFirstSearch.cs
@@ -1,8 +1,93 @@
 using System;
 
-namespace src{
-    public partial class Algorithms{
-        public void BreathFirstSearch(){
+namespace src
+{
+    public partial class Algorithms
+    {
+        public List<Cell> BreathFirstSearch(Graph graph)
+        {
+            var checkedCells = new List<Cell>();
+
+            var start = graph.EntryVertex;
+
+            var solutionSpace = new List<Cell>();
+            solutionSpace.Add(start);
+
+            var checkQueue = new Queue<Cell>();
+            checkQueue.Enqueue(start);
+
+            while (checkQueue.Count > 0)
+            {
+                var currCell = checkQueue.Dequeue();
+
+                if (checkedCells.Contains(currCell))
+                {
+                    continue;
+                }
+
+                if (currCell.Type == 9 && !solutionSpace.Contains(currCell))
+                {
+                    solutionSpace.Add(currCell);
+                }
+
+                checkedCells.Add(currCell);
+
+                foreach (var neighbor in graph.GetCellNeighbors(currCell))
+                {
+                    if (!checkedCells.Contains(neighbor))
+                    {
+                        checkQueue.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            return solutionSpace;
+        }
+
+        public Func<Cell> ShortestPath(Graph graph)
+        {
+            var route = new Dictionary<Cell, Cell>();
+
+            var cellQueue = new Queue<Cell>();
+            cellQueue.Enqueue(graph.EntryVertex);
+
+            while (cellQueue.Count > 0)
+            {
+                var currCell = cellQueue.Dequeue();
+
+                foreach (var neighbor in graph.GetCellNeighbors[currCell])
+                {
+                    if (route.ContainsKey(neighbor))
+                    {
+                        continue;
+                    }
+
+                    route[neighbor] = currCell;
+                    cellQueue.Enqueue(neighbor);
+                }
+            }
+
+            Func<Cell> shortestPath = v =>
+            {
+                var path = new List<Cell>();
+
+                for (int i = v.Count; i > 0; i--)
+                {
+                    var curr = v.ElementAt(i);
+                    while (!curr.Equals(v.ElementAt(i - 1)))
+                    {
+                        path.Add(curr);
+                        curr = route[curr];
+                    }
+                }
+
+                path.Add(graph.EntryVertex);
+                path.Reverse();
+
+                return path;
+            };
+
+            return shortestPath;
         }
     }
 }
